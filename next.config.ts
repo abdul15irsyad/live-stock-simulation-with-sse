8484497e39d55d314458a9fe2ac10d143ec268e0
Irsyad Abdul <abdulirsyad15@gmail.com>
2026-03-22 00:14:43 +0700

    feat: uncomment next config

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/merge',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/merge',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
